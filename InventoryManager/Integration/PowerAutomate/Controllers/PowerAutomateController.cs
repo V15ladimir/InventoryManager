3a88d6d913239efd54e54810b0c857f32bc301ec
V15ladimir <vprihodko1sss@gmail.com>
2026-06-01 22:58:36 +0300

    Completed PA integration

--- InventoryManager/Integration/PowerAutomate/Controllers/PowerAutomateController.cs
+++ InventoryManager/Integration/PowerAutomate/Controllers/PowerAutomateController.cs
@@ -1,10 +1,7 @@
 ﻿using InventoryManager.Integration.PowerAutomate.Models;
 using InventoryManager.Integration.PowerAutomate.Services;
-using InventoryManager.Integration.Salesforce.Models;
-using InventoryManager.Integration.Salesforce.Services;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace InventoryManager.Integration.PowerAutomate.Controllers {
 
@@ -12,10 +9,10 @@ namespace InventoryManager.Integration.PowerAutomate.Controllers {
     [Route("api/[controller]")]
     public class PowerAutomateController(IDropBoxService dropBoxService) : ControllerBase {
 
-        [HttpPost]
+        [HttpPost("create")]
         public async Task<IActionResult> Create([FromBody] SupportTicketModel model) {
             var content = JsonConvert.SerializeObject(model, Formatting.Indented);
-            await dropBoxService.UploadFileAsync(content, $"SupportTicket_{Guid.NewGuid()}");
+            await dropBoxService.UploadFileAsync(content, $"{Guid.NewGuid()}");
             return Ok();
         }
     }
